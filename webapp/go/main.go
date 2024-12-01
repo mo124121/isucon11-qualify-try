0fcb77a245c7b85e3f415c4b441a1b6c22d2555b
mo124121 <101929016+mo124121@users.noreply.github.com>
2024-12-01 15:16:24 +0900

    limit select 25448(25450 - 2) : pass

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1174,7 +1174,7 @@ func getTrend(c echo.Context) error {
 		for _, isu := range isuList {
 			conditions := []IsuCondition{}
 			err = db.SelectContext(ctx, &conditions,
-				"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC",
+				"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC LIMIT 1",
 				isu.JIAIsuUUID,
 			)
 			if err != nil {
